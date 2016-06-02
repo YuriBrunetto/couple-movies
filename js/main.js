5bbcb35608978d53149b2288dd6108e844747b69
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-06-01 23:45:05 -0300

    close the tips section

--- js/main.js
+++ js/main.js
@@ -8,4 +8,21 @@ $(function() {
         $("#menu-close").toggleClass("active");
     });
 
+    // close tips
+    var isClosed = window.localStorage.getItem("CoupleMoviesTips");
+
+    if (isClosed) {
+        $(".tips-container").remove();
+    } else {
+        $("#close-tips").click(function() {
+            $(".tips-container").css("opacity", 0);
+            $(".tips-container").one("webkitTransitionEnd mozTransitionEnd MSTransitionEnd oatransitionend transitionend", function() {
+                $(".tips-container").hide("slow", function() { $(".tips-container").remove(); });
+            });
+
+            var closed = true;
+            window.localStorage.setItem("CoupleMoviesTips", closed);
+        });
+    }
+
 });
