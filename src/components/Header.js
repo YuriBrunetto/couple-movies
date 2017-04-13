9246ca97026d2fe6c2fc0b5f495c6dc2706d38d8
Yuri Brunetto <brunettoyuri@gmail.com>
2017-04-13 03:45:39 -0300

    remove addons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,9 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Link } from 'react-router-dom'
 import '../styles/Header.css'
-import DropdownMenu from 'react-dd-menu'
 
 class Header extends Component {
-  constructor() {
-    super()
-    this.state = { isMenuOpen: false }
-  }
-
-  toggle = () => {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen })
-  }
-  close = () => {
-    this.setState({ isMenuOpen: false })
-  }
-  click = () => {
-    console.log('clicked an item')
-  }
-
   render() {
-    let menuOptions = {
-      isOpen: this.state.isMenuOpen,
-      close: this.close.bind(this),
-      toggle:
-        <button type="button" className="header-btn" onClick={this.toggle.bind(this)}>
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </button>,
-      align: 'left'
-    }
-
     return (
       <header className="header">
         <div className="header-wrap">
@@ -41,9 +15,6 @@ class Header extends Component {
             <button type="button" className="header-btn">
               <i className="fa fa-search" aria-hidden="true"></i>
             </button>
-            <DropdownMenu {...menuOptions}>
-              
-            </DropdownMenu>
           </div>
         </div>
       </header>
